ac183b35be0a58fa41e51665de221bb044fa5ca4
Clark Wagdy <clarkwagdy1@gmail.com>
2021-04-01 19:19:11 +0200

    Project termination

--- JS/Index.js
+++ JS/Index.js
@@ -1,7 +1,7 @@
 // function for droplist
 $(document).ready(function () {
-  console.log(window);
-
+  Angeladata();
+  slide1();
   $("#showlist").mouseenter(function () {
     $(".tdroplist").animate({ margin: "10px 0 0 60px", opacity: "1" }).show();
   });
@@ -9,6 +9,18 @@ $(document).ready(function () {
   $(".tdroplist").mouseleave(function () {
     $(".tdroplist")
       .animate({ margin: "30px 0 0 60px", opacity: "0" })
+      .hide(500);
+  });
+
+  $("#showlist2").mouseenter(function () {
+    $(".tdroplist2")
+      .animate({ margin: "-10px 0 0 120px", opacity: "1" })
+      .show();
+  });
+
+  $(".tdroplist2").mouseleave(function () {
+    $(".tdroplist2")
+      .animate({ margin: "-20px 0 0 120px", opacity: "0" })
       .hide(500);
   });
 });
@@ -42,3 +54,71 @@ function check_if_in_view() {
 
 $window.on("scroll resize", check_if_in_view);
 $window.trigger("scroll");
+
+function Angeladata() {
+  var xhttp = new XMLHttpRequest();
+  console.log(xhttp);
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("data").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "html/AngelaSmith.html", true);
+  xhttp.send();
+}
+function clarkdata() {
+  var xhttp = new XMLHttpRequest();
+  console.log(xhttp);
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("data").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "html/ClarkWagdy.html", true);
+  xhttp.send();
+}
+function monadata() {
+  var xhttp = new XMLHttpRequest();
+  console.log(xhttp);
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("data").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "html/MonaZaki.html", true);
+  xhttp.send();
+}
+function slide1() {
+  var xhttp = new XMLHttpRequest();
+  console.log(xhttp);
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("slide1").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "html/slide1.html", true);
+  xhttp.send();
+}
+function slide2() {
+  var xhttp = new XMLHttpRequest();
+  console.log(xhttp);
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("slide1").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "html/slide2.html", true);
+  xhttp.send();
+}
+function slide3() {
+  var xhttp = new XMLHttpRequest();
+  console.log(xhttp);
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("slide1").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "html/slide3.html", true);
+  xhttp.send();
+}
+
